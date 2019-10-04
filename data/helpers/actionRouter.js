0f92fadbc986631591fa3d63e527f8fd08760266
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-10-04 15:24:09 -0500

    routers updated

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,89 +1,59 @@
 const express = require('express');
+const action = require('../data/helpers/actionModel');
 
- const actions = require('./actionModel.js');
-const project = require('./projectModel.js');
- const router = express.Router()
+const router = express.Router();
 
- //get actions from id
-router.get('/:id', validateaction, (req, res) => {
-    actions.get(req.action)
-        .then(action => {
-            res.status(200).json(action)
-        })
-        .catch(() => {
-            res.status(500).json({Error: 'could not retrive the actions from the database'})
-        })
-})
+router.get('/', (req, res) => {
+  action.get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .error(err => {
+      console.log(err)
+      res.status(500).json({ error: "Error getting actions." })
+    })
+});
 
-//post an actions
-router.post("/:id/actions", validatePost, (req, res) => {
-    const {description, notes} = req.body;
-    req.body.project_id = req.project
-    
-    if (!description || !notes) {
-        res.status(400).json({Denied: `You must provide a description and notes`})
-    } else {
-        actions.insert(req.body)
-            .then(action => {
-                res.status(201).json(action)
-            })
-            .catch(err => {
-                res.status(500).json({Error: 'there was an issue createing the actions for the project'})
-            })
-    }
-})
+router.post('/', (req, res) => {
+  const actions = req.body;
+  action.insert(actions)
+    .then(actions => {
+      res.status(201).json(actions);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error inserting actions.' })
+    })
+});
 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  action.remove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error deleting actions.' })
+    })
+});
 
-router.delete("/:id", validateaction, (req, res) => {
-    actions.remove(req.action)
-        .then(num => {
-            res.status(200).json({Sucess: `${num} items were sucessfully deleted`})
-        })
-        .catch(() => {
-            res.status(500).json({Error: "there was an error deleteing the action from the database"})
-        })
-})
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { project_id, description, notes, completed } = req.body;
 
-router.put("/:id", validateaction, (req, res) => {
-    const {description, notes} = req.body;
-    if (!description || !notes) {
-        res.status(400).json({Denied: `You must provide a description and notes`})
-    } else (
-        actions.update(req.action, req.body)
-            .then(action => {
-                res.status(200).json(action)
-            })
-            .catch(() => {
-                res.status(500).json({Error: 'there was an error updateing the actions'})
-            })
-    )
+  action.update(id, { project_id, description, notes, completed })
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error updating actions.' })
+    })
 })
 
 //check and see if the project exists
-function validatePost(req, res, next) {
-    project.get(req.params.id)
-        .then(got => {
-            if (!got) {
-                res.status(400).json({Error: "there is no project with that id"})
-            } else {
-                req.project = req.params.id
-                next()
-            }
-        })
-}
 
-function validateaction(req, res, next) {
-    actions.get(req.params.id)
-        .then(got => {
-            if (!got) {
-                res.status(400).json({Error: "there is no actions with that id"})
-            } else {
-                req.body.project_id = got.project_id
-                req.action = req.params.id
-                next()
-            }
-        })
-}
 
 
  module.exports = router
--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -1,72 +1,71 @@
-const express = require('express')
+const express = require('express');
+const project = require('../data/helpers/projectModel');
 
-const project = require('./projectModel.js')
+const router = express.Router();
 
-const router = express.Router()
-//get all project
 router.get('/', (req, res) => {
-    project.get()
-    .then(result => {
-        res.status(200).json(result)
+  project.get()
+    .then(projects => {
+      res.status(200).json(projects);
     })
-    .catch(error => {
-        res.status(500).json({error: "The projects information could not be retrieved."})
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error getting projects.' });
+    })
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  project.getProjectActions(id)
+    .then(projects => {
+      if (projects) {
+        res.status(200).json(projects)
+      } else {
+        res.status(404).json({ error: 'Projects with id does not match.' })
+      }
+    })
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+
+  project.update(id, { name, description })
+    .then(updated => {
+      res.status(201).json(updated);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error updatig project.' })
     })
 })
 
-//get project actions
-router.get('/:id', validatePost, (req, res) => {
-    const {id} = req.params
-    project.getProjectActions(id)
-    .then(results => {
-        res.status(200).json(results)
+router.post('/', validatePost, (req, res) => {
+  const project = req.body;
+  project.insert(project)
+    .then(project => {
+      res.status(201).json(project);
     })
-    .catch(error => {
-        res.status(500).json({error: "The projects information could not be retrieved."})
-    })
-})
-//post a project
-router.post('/', (req, res) => {
-    const {name, description} = req.body
-    if (!name || !description) {
-        res.status(400).json({Error: 'You must provide both a name and description'})
-    } else {
-        project.insert(req.body)
-            .then(post => {
-                res.status(201).json(post)
-            })
-            .catch(() => {
-                res.status(500).json({Error: 'there was an error adding the post to the database'})
-            })
-    }
-})
-//delete a project
-router.delete("/:id", (req, res) => {
-    const {id} = req.params
-    project.remove(id)
-    .then(result => {
-        res.status(200).json({message: 'project deleted succesfully'})
-    })
-    .catch(error => {
-        res.status(500).json({error: "The projects information could not be removed."})
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error inserting project.' })
     })
 })
 
-//edit a project
-router.put("/:id", validatePost, (req,res) => {
-    const {name, description} = req.body
-    if (!name || !description) {
-        res.status(400).json({Error: 'You must provide both a name and description'})
-    } else {
-        project.update(req.project, req.body)
-            .then(post => {
-                res.status(200).json(post)
-            })
-            .catch(() => {
-                res.status(500).json({Error: 'there was an error updateing the post in the projectbase'})
-            })
-    }
-})
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  project.remove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error deleting projects.' })
+    })
+});
+
+module.exports = router;
 //check and see if the project exists
 function validatePost(req, res, next) {
     project.get(req.params.id)
